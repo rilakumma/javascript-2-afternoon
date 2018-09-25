4b3cbb3e6f4d60ac8895a7156608ba451ba24e94
Madison Walmsley <madi@Madisons-MacBook-Pro.local>
2018-09-25 15:22:07 -0700

    finished practice but not extra practice questions

--- practice.js
+++ practice.js
@@ -14,9 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+var me = {
+  name: 'madi',
+  age: 23,
+}
 
-
+alert(me.name);
 
 ////////// PROBLEM 2 //////////
 
@@ -25,7 +28,15 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+var favoriteThings = {
+  band: 'my bloody valentine',
+  food: 'veggie burger',
+  person: 'my mom',
+  book: '1q84',
+  movie: 'perfect blue',
+  holiday: 'halloween'
+
+}
 
 
 
@@ -33,7 +44,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'honda fit';
+favoriteThings.brand = 'american apparel';
 
 
 
@@ -42,7 +54,8 @@
 */
 
 //Code here
-
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 
 ////////// PROBLEM 3 //////////
@@ -56,6 +69,12 @@
 */
 
 //Code here
+var backPack = {
+
+}
+var item = 'firstPocket';
+backPack['firstPocket'] = 'chapstick';
+backPack.color = 'red';
 
 
 
@@ -63,7 +82,8 @@
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert(backPack);
+
 
 
 
@@ -74,6 +94,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
+console.log(backPack);
 
 
 
@@ -97,6 +118,8 @@ var user2 = {
 */
 
 //Code Here
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
